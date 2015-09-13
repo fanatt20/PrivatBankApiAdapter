6cc88eef4f1cd1439642793293c177e1a6f6aa1d
Andrey Venzhyk <fanatt20@gmail.com>
2015-09-13 20:25:07 +0300

    add comments

--- ApiPrivatBankAdapter/AccountFactory.cs
+++ ApiPrivatBankAdapter/AccountFactory.cs
@@ -13,6 +13,10 @@ namespace ApiPrivatBankAdapter
         {
 
             var passwordInMd5 = MD5.Create().ComputeHash(PhpEncoding.GetByteArray("password"));
+            /*
+             * In api, password transmitted as sha1(md5($data.$password)) but md5() return string,
+             * for this reason we need to cast the result of md5() to string as byte array and then use sha1()
+             */
             var passwordInMd5AsString = BitConverter.ToString(passwordInMd5).Replace("-", string.Empty).ToLower();
             var passwordInSha1AndMd5 = SHA1.Create().ComputeHash(PhpEncoding.GetByteArray(passwordInMd5AsString));
             return new Account(name, BitConverter.ToString(passwordInSha1AndMd5).Replace("-",string.Empty).ToLower());
