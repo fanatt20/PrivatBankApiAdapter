c280f00cdca25105e75386ee564c30698807d641
Andrey Venzhyk <fanatt20@gmail.com>
2015-11-01 17:36:54 +0200

    optimization string concatination

--- PrivatBankApiWrapper/Request/DataProperties/Payment.cs
+++ PrivatBankApiWrapper/Request/DataProperties/Payment.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace PrivatBankApiWrapper.Request.DataProperties
 {
     internal class Payment : IDataProperty
     {
-        internal int? Id { get; set; }
         public Payment()
         {
             AttributesDictionary = new Dictionary<string, string>();
         }
 
-        internal Dictionary<string, string> AttributesDictionary { get; private set; }
+        internal int? Id { get; set; }
+
+        internal Dictionary<string, string> AttributesDictionary { get; }
 
         public string GetXml()
         {
-            return "<payment id=\"" + (Id != null ? Id.ToString() : "") + "\">" + AttributesDictionary.Aggregate(string.Empty,
-                (key, value) => "<prop name=\"" + key + "\" value=\"" + value + "\" />") + "</payment>";
+            return "<payment id=\"" + (Id != null ? Id.ToString() : "") + "\">" +
+                   AttributesDictionary.Aggregate(new StringBuilder(),
+                       (builder, pair) =>
+                           builder.Append("<prop name=\"" + pair.Key + "\" value=\"" + pair.Value + "\" />"),
+                       builder => builder.ToString()) + "</payment>";
+            ;
         }
     }
 }
